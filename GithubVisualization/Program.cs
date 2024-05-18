bf598c48815ca0e223b4f7234dafb7d04f0cb0aa
Nemanja Milenkovic <nemanjamilenkovic@live.com>
2024-05-18 23:34:00 +0200

    Initial piece of functionality
    - Add a code to Program.cs that lists all of the commits for a repo supplied by the user
    - Add gitignore for .net projects

--- GithubVisualization/Program.cs
+++ GithubVisualization/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using LibGit2Sharp;
+
+Console.WriteLine("Enter the path to the repository: ");
+string repoPath = Console.ReadLine();
+
+using (var repo = new Repository(repoPath))
+{
+    foreach (var commit in repo.Commits)
+    {
+        Console.WriteLine($"Commit message: {commit.Message}");
+    }
+}
