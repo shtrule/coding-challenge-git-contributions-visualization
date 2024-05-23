67b06e98ba2f80ac7f3b38771d951550947f8f0e
Nemanja Milenkovic <nemanjamilenkovic@live.com>
2024-05-23 21:55:25 +0200

    Add total number of contributions to the output

--- GithubVisualization/Program.cs
+++ GithubVisualization/Program.cs
@@ -12,7 +12,8 @@ if (string.IsNullOrWhiteSpace(repoPath) || !Directory.Exists(repoPath))
 Console.WriteLine("Enter the number of days to look back for commits: ");
 var numberOfDaysRaw = Console.ReadLine();
 
-if (!int.TryParse(numberOfDaysRaw, out var numberOfDays)) {
+if (!int.TryParse(numberOfDaysRaw, out var numberOfDays))
+{
     Console.WriteLine("Invalid number of days.");
     return;
 }
@@ -20,41 +21,67 @@ if (!int.TryParse(numberOfDaysRaw, out var numberOfDays)) {
 
 if (Repository.IsValid(repoPath))
 {
-    PrintCurrentUserStatsForRepo(repoPath, numberOfDays);
+    using (var repo = new Repository(repoPath))
+    {
+        var repoStats = GetCurrentUserStatsForRepo(repo, numberOfDays);
+        PrintCurrentUserStatsForRepo(repoStats, repo.Info.WorkingDirectory);
+    }
     return;
 }
+
+var dict = new Dictionary<string, int>();
 
 foreach (var subDir in Directory.GetDirectories(repoPath))
 {
     if (Repository.IsValid(subDir))
     {
-        PrintCurrentUserStatsForRepo(subDir, numberOfDays);
+        using (var repo = new Repository(subDir))
+        {
+            var repoStats = GetCurrentUserStatsForRepo(repo, numberOfDays);
+            foreach (var entry in repoStats)
+            {
+                if (dict.ContainsKey(entry.Key))
+                {
+                    dict[entry.Key] += entry.Value;
+                }
+                else
+                {
+                    dict[entry.Key] = entry.Value;
+                }
+            }
+            PrintCurrentUserStatsForRepo(repoStats, repo.Info.WorkingDirectory);
+        }
     }
 }
 
-void PrintCurrentUserStatsForRepo(string repoPath, int numberOfDays)
+PrintCurrentUserStatsForRepo(dict, "Total contributions");
+
+Dictionary<string, int> GetCurrentUserStatsForRepo(Repository repo, int numberOfDays)
 {
     var dict = new Dictionary<string, int>();
-    using (var repo = new Repository(repoPath))
-    {
-        foreach (var commit in repo.Commits.Where(c => c.Author.When > DateTime.Now.AddDays(-numberOfDays)))
-        {
-            if (dict.ContainsKey(commit.Author.Email))
-            {
-                dict[commit.Author.Email]++;
-            }
-            else
-            {
-                dict[commit.Author.Email] = 1;
-            }
-        }
 
-        if (dict.Count > 0) {
-            var repoName = new DirectoryInfo(repo.Info.WorkingDirectory).Name;
-            foreach (var entry in dict)
-            {
-                Console.WriteLine($"Number of commits by {entry.Key} in repo {repoName} the last {numberOfDays} days: {entry.Value}");
-            }
+    foreach (var commit in repo.Commits.Where(c => c.Author.When > DateTime.Now.AddDays(-numberOfDays)))
+    {
+        if (dict.ContainsKey(commit.Author.Email))
+        {
+            dict[commit.Author.Email]++;
+        }
+        else
+        {
+            dict[commit.Author.Email] = 1;
+        }
+    }
+
+    return dict;
+}
+
+void PrintCurrentUserStatsForRepo(Dictionary<string, int> dict, string repoName)
+{
+    if (dict.Count > 0)
+    {
+        foreach (var entry in dict)
+        {
+            Console.WriteLine($"Number of commits by {entry.Key} in repo {repoName} the last {numberOfDays} days: {entry.Value}");
         }
     }
 }
