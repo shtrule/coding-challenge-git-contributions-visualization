d2537d9d08021bce2b291d34982e629030b42920
Nemanja Milenkovic <nemanjamilenkovic@live.com>
2024-05-26 21:02:52 +0200

    Add react calendar heatmap and start with the implementation of the contributions graph on the frontend

--- GitVisualizationFrontend/git-visualization/src/App.js
+++ GitVisualizationFrontend/git-visualization/src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CalendarHeatmap from 'react-calendar-heatmap';
+import Popup from 'reactjs-popup';
 
 function App() {
   const [data, setData] = useState(null);
@@ -11,6 +13,28 @@ function App() {
       .then(data => setData(data));
   }, []);
 
+  
+  let heatmapData = [];
+  // let startDate = new Date();
+  // let endDate = new Date();
+  if (data) {
+      let userEmail = 'nemanjamilenkovic@live.com';
+      let userContributions = data[userEmail];
+
+      if (userContributions) {
+        console.log(userContributions.totalContributions);
+        heatmapData = userContributions.totalContributions
+        .filter(contribution => contribution.Date)
+        .map(contribution => ({
+          date: new Date(contribution.Date).toISOString().slice(0, 10),
+          count: contribution.numberOfContributions,
+        }));
+        
+      }
+        // startDate = new Date(Math.min(...heatmapData.map(d => new Date(d.date))));
+        // endDate = new Date(Math.max(...heatmapData.map(d => new Date(d.date))));
+    }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -19,6 +43,12 @@ function App() {
           Edit <code>src/App.js</code> and save to reload you fool.
         </p>
         {data && <p>Data from API: {JSON.stringify(data)}</p>}
+        {data && <CalendarHeatmap
+          startDate={new Date().setFullYear(new Date().getFullYear() - 1)}
+          endDate={new Date()}
+          values={heatmapData}
+          // ... other props
+        />}
         <a
           className="App-link"
           href="https://reactjs.org"
